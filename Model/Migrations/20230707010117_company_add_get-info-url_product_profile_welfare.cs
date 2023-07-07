ba10cda40adc3e68f4b47eeb2cee5c351248c56d
ari <arisuokayokay@gmail.com>
2023-07-07 09:01:37 +0800

    公司資料表欄位增加欄位

---
+++ Model/Migrations/20230707010117_company_add_get-info-url_product_profile_welfare.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    public partial class company_add_getinfourl_product_profile_welfare : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "get_info_url",
+                schema: "jobseeker",
+                table: "company",
+                type: "text",
+                nullable: true,
+                defaultValueSql: "''::text",
+                comment: "取得資料的網址");
+
+            migrationBuilder.AddColumn<string>(
+                name: "product",
+                schema: "jobseeker",
+                table: "company",
+                type: "text",
+                nullable: true,
+                comment: "主要商品/服務");
+
+            migrationBuilder.AddColumn<string>(
+                name: "profile",
+                schema: "jobseeker",
+                table: "company",
+                type: "text",
+                nullable: true,
+                comment: "公司描述");
+
+            migrationBuilder.AddColumn<string>(
+                name: "welfare",
+                schema: "jobseeker",
+                table: "company",
+                type: "text",
+                nullable: true,
+                comment: "福利");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "get_info_url",
+                schema: "jobseeker",
+                table: "company");
+
+            migrationBuilder.DropColumn(
+                name: "product",
+                schema: "jobseeker",
+                table: "company");
+
+            migrationBuilder.DropColumn(
+                name: "profile",
+                schema: "jobseeker",
+                table: "company");
+
+            migrationBuilder.DropColumn(
+                name: "welfare",
+                schema: "jobseeker",
+                table: "company");
+        }
+    }
+}
