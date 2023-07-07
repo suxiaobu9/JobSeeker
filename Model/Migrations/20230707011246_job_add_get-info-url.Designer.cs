dcb7658f9590c91e9f95834c9c670c799be4c162
ari <arisuokayokay@gmail.com>
2023-07-07 09:13:19 +0800

    Job 資料欄位增加 GetInfoUrl 欄位

--- Model/Migrations/20230707011246_job_add_get-info-url.Designer.cs
+++ Model/Migrations/20230707011246_job_add_get-info-url.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Model.JobSeekerDb;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Model.Migrations
 {
     [DbContext(typeof(postgresContext))]
-    partial class postgresContextModelSnapshot : ModelSnapshot
+    [Migration("20230707011246_job_add_get-info-url")]
+    partial class job_add_getinfourl
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
