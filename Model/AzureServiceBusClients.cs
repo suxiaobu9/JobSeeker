b4ac426400d5e8afdb3e030709693f207080b8de
ari <arisuokayokay@gmail.com>
2023-07-17 20:12:43 +0800

    service bus DI 用的 model

---
+++ Model/AzureServiceBusClients.cs
@@ -0,0 +1,20 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model;
+
+public class AzureServiceBusClients
+{
+    public readonly ServiceBusClient JobQueue;
+    public readonly ServiceBusClient CompanyQueue;
+
+    public AzureServiceBusClients(ServiceBusClient jobQueue, ServiceBusClient companyQueue)
+    {
+        this.JobQueue = jobQueue;
+        this.CompanyQueue = companyQueue;
+    }
+}
