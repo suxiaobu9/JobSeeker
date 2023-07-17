1b4fc053a9383f8849018497652b82b1bbf54c66
ari <arisuokayokay@gmail.com>
2023-07-17 20:12:25 +0800

    設定 interface

---
+++ Service/Cache/ICacheService.cs
@@ -0,0 +1,19 @@
+﻿namespace Service.Cache;
+
+public interface ICacheService
+{
+    /// <summary>
+    /// 重置已存在的公司與工作
+    /// </summary>
+    /// <returns></returns>
+    public Task ResetExistCompanyAndJob();
+
+    /// <summary>
+    /// key field 是否存在於 cache 中
+    /// </summary>
+    /// <param name="key"></param>
+    /// <param name="field"></param>
+    /// <returns></returns>
+    public Task<bool> IsKeyFieldExistsInCache(string key, string field);
+
+}
---
+++ Service/Http/IHttpService.cs
@@ -0,0 +1,30 @@
+﻿using Model.Dto;
+
+namespace Service.Http;
+
+public interface IHttpService
+{
+    /// <summary>
+    /// 取得職缺清單
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <returns></returns>
+    public Task<IEnumerable<T>> GetJobList<T>(string url) where T : JobListDto;
+
+    /// <summary>
+    /// 取得職缺資訊
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public Task<T> GetJobInfo<T>(string url) where T : JobDto;
+
+    /// <summary>
+    /// 取得公司資訊
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="url"></param>
+    /// <returns></returns>
+    public Task<T> GetCompanyInfo<T>(string url) where T : CompanyDto;
+
+}
