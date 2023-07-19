334089f08160885523265514beef7d8596e4da94
ari <arisuokayokay@gmail.com>
2023-07-19 19:29:45 +0800

    調整 PK

---
+++ Model/Migrations/20230719113024_mdf_job_table_pk.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Model.Migrations
+{
+    public partial class mdf_job_table_pk : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_job",
+                schema: "jobseeker",
+                table: "job");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "job_pk",
+                schema: "jobseeker",
+                table: "job",
+                columns: new[] { "id", "company_id" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "job_pk",
+                schema: "jobseeker",
+                table: "job");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_job",
+                schema: "jobseeker",
+                table: "job",
+                column: "id");
+        }
+    }
+}
