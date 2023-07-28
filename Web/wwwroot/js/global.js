d46501a8c997db0e4ccdd6d7fe5a1312c986ac6d
ari <arisuokayokay@gmail.com>
2023-07-28 17:05:25 +0800

    加入快捷鍵來已讀、忽略公司及職缺資訊

--- Web/wwwroot/js/global.js
+++ Web/wwwroot/js/global.js
@@ -1,7 +1,16 @@
 window.registerShortcut = (dotNetReference) => {
     document.addEventListener('keydown', (event) => {
-        //if (event.key === 'x') {
-        //    dotNetReference.invokeMethodAsync('IgnoreCompany');
-        //}
+        if (event.key === 'q') {
+            dotNetReference.invokeMethodAsync('ReadAllJobs');
+        }
+        if (event.key === 'w') {
+            dotNetReference.invokeMethodAsync('IgnoreCompany');
+        }
+        if (event.key === 'a') {
+            dotNetReference.invokeMethodAsync('ReadFirstJob');
+        }
+        if (event.key === 's') {
+            dotNetReference.invokeMethodAsync('IgnoreFirstJob');
+        }
     });
 }
