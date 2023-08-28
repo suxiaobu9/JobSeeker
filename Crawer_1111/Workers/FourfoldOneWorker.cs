d7419db7a1300baef55648422aee2c9132d95f4b
ari <arisuokayokay@gmail.com>
2023-08-28 20:09:10 +0800

    基本的引用與注入

---
+++ Crawer_1111/Workers/FourfoldOneWorker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crawer_1111.Workers;
+
+public class FourfoldOneWorker : BackgroundService
+{
+    private readonly ILogger<FourfoldOneWorker> logger;
+
+    public FourfoldOneWorker(ILogger<FourfoldOneWorker> logger)
+    {
+        this.logger = logger;
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        throw new NotImplementedException();
+    }
+}
